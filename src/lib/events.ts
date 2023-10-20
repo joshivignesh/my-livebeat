7e1accfbab8a01b6708599e0e81e5e1e40e02040
Vignesh <joshivignesh@users.noreply.github.com>
2023-10-20 22:46:04 +0530

    Added Delete Event

--- src/lib/events.ts
+++ src/lib/events.ts
@@ -1,5 +1,5 @@
 import { Models, ID } from 'appwrite';
-import { database } from '@/lib/appwrite';
+import { database, storage } from '@/lib/appwrite';
 // import { deleteFileById } from '@/lib/storage';
 import { LiveBeatEvent } from '@/types/events';
 
@@ -17,19 +17,24 @@ export async function getEventById(eventId: LiveBeatEvent['$id']) {
   }
 }
 
-// export async function deleteEventById(eventId: LiveBeatEvent['$id']) {
-//   const { event } = await getEventById(eventId);
-//   if ( event.imageFileId ) {
-//     await deleteFileById(event.imageFileId)
-//   }
-//   await database.deleteDocument(import.meta.env.VITE_APPWRITE_EVENTS_DATABASE_ID, import.meta.env.VITE_APPWRITE_EVENTS_COLLECTION_ID, eventId);
-// }
+export async function deleteEventById(eventId: LiveBeatEvent['$id']) {
+  const { event } = await getEventById(eventId);
+  if ( event.imageFileId ) {
+    await deleteFileById(event.imageFileId)
+  }
+  await database.deleteDocument(import.meta.env.VITE_APPWRITE_EVENTS_DATABASE_ID, import.meta.env.VITE_APPWRITE_EVENTS_COLLECTION_ID, eventId);
+}
 
 export async function createEvent(event: Omit<LiveBeatEvent, '$id'>) {
   const document = await database.createDocument(import.meta.env.VITE_APPWRITE_EVENTS_DATABASE_ID, import.meta.env.VITE_APPWRITE_EVENTS_COLLECTION_ID, ID.unique(), event);
   return {
     event: mapDocumentToEvent(document)
   }
+}
+
+export async function deleteFileById(fileId: string) {
+  const data = await storage.deleteFile(import.meta.env.VITE_APPWRITE_EVENTS_BUCKET_IMAGES_ID, fileId)
+  return data;
 }
 
 function mapDocumentToEvent(document: Models.Document) {
